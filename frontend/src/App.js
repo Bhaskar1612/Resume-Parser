01c85bdf41445f448b03226d3bd5e6c5ac456770
Bhaskar1612 <bhaskarkashyap1612@gmail.com>
2025-03-18 03:52:05 +0530

    final_commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,21 @@ import "./index.css";
 function App() {
   return (
     <Router>
-      <div className="homepage-container">
-        <h1 className="homepage-title">Resume Extractor</h1>
-        <div className="homepage-buttons">
-          <Link to="/upload" className="homepage-button">Extract Resume</Link>
-          <Link to="/search" className="homepage-button">Search Resume</Link>
+      <div className="app-container"> {/* Box Wrapper */}
+        <div className="homepage-container">
+          <h1 className="homepage-title">Resume Extractor</h1>
+          <div className="homepage-buttons">
+            <Link to="/upload" className="homepage-button">Extract Resume</Link>
+            <Link to="/search" className="homepage-button">Search Resume</Link>
+          </div>
         </div>
+        <Routes>
+          <Route path="/upload" element={<UploadForm />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/search-result" element={<SearchResult />} />
+          <Route path="/thank-you" element={<ThankYouPage />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/upload" element={<UploadForm />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/search-result" element={<SearchResult />} />
-        <Route path="/thank-you" element={<ThankYouPage />} />
-      </Routes>
     </Router>
   );
 }
